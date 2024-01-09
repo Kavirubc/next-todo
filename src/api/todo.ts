2fa243ebf9ad10ef0a9846e819132657218ca5d0
Kaviru Hapuarachchi <hapuarachchikaviru@gmail.com>
2024-01-09 23:44:24 +0530

    Add form data to send function

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -1,5 +1,10 @@
 "use server"
 
-export async function send() {
+export async function send(formData: FormData) {
+
+    const rawFormData = {
+        todo: formData.get('todo'),
+    }
     console.log("send");
+    console.log(rawFormData.todo);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import { send } from "../api/todo";
 
+
 export default function Home() {
+
+
+
+
   return (
     <>
       <div className="h-screen flex flex-col items-center justify-center">
         <h1 className="text-2xl">Todo</h1>
         <div className="my-5">
           <form className="flex flex-col" action={send}>
-            <input className="border-2 border-black max-w-xs" type="text" placeholder="Add todo" />
+            <input id="todo" name="todo" className="border-2 border-black max-w-xs" type="text" placeholder="Add todo" />
             <button className="border-2 border-black p-2 bg-indigo-400 mt-4 max-w-xs">Add</button>
           </form>
         </div>
