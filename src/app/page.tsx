ec675e654ddf10e291866478d404a57f86fc336d
Kaviru Hapuarachchi <hapuarachchikaviru@gmail.com>
2024-01-09 22:36:18 +0530

    Add todo functionality to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import { send } from "../api/todo";
+
 export default function Home() {
   return (
     <>
-    <h1>Hello</h1>
+      <div className="h-screen flex flex-col items-center justify-center">
+        <h1 className="text-2xl">Todo</h1>
+        <div className="my-5">
+          <form className="flex flex-col" action={send}>
+            <input className="border-2 border-black max-w-xs" type="text" placeholder="Add todo" />
+            <button className="border-2 border-black p-2 bg-indigo-400 mt-4 max-w-xs">Add</button>
+          </form>
+        </div>
+        <div>
+          The list of todos will be here
+        </div>
+      </div>
     </>
   )
 }
